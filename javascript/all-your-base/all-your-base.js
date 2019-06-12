a82d2a6acb7018e069f3622224f62e2d0665d555
Joe Previte <jjprevite@gmail.com>
2019-06-12 16:52:20 -0700

    Add functions to help with all-your-base

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -26,4 +26,47 @@ export function convert (n, inputBase, outputBase) {
   if (n.length === 0) {
     throw Error('Input has wrong format')
   }
+
+  /*
+    TODO
+    2. figure out how to solve it.
+  */
+  const output = []
+  const length = n.length
+  // Start with a loop and count backwards
+  for (let i = length - 1; i >= 0; i--) {
+    // const result = Math.pow(inputBase, i) - Math.pow(outputBase, 1)
+  }
 }
+
+function getMaxPower (integer, base) {
+  let copiedInt = integer
+  let maxPower = 0
+
+  for (let i = 0; copiedInt >= Math.pow(base, i); i++) {
+    maxPower = i
+  }
+  return maxPower
+}
+
+function calculateDigits (integer, base, maxPower, digits) {
+  if (maxPower === 0) {
+    return [...digits, integer]
+  }
+  // Clone maxPower
+  let copiedMaxPower = maxPower
+  // Calculate digit
+  const digit = Math.floor(integer / Math.pow(base, maxPower))
+  // Get remainder
+  const remainder = integer % base
+  // Store in copiedDigits
+  const copiedDigits = [...digits, digit]
+  copiedMaxPower -= 1
+
+  if (copiedMaxPower >= 0) {
+    return calculateDigits(remainder, base, copiedMaxPower, copiedDigits)
+  }
+  return copiedDigits
+}
+
+// Next step, testing calculate digits with a number bigger than 16.
