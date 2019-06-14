f9824a02663eae0f60000839323aab53539a343f
Joe Previte <jjprevite@gmail.com>
2019-06-14 07:43:29 -0700

    Finish hamming challenge

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,23 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (strandA, strandB) => {
+
+  // Check left strand
+  if (strandA === '' && strandB !== '' || strandA !== '' && strandB === '') {
+    throw new Error(`${strandA === '' ? 'left' : 'right'} strand must not be empty`)
+  }
+
+  // Check for equal length
+  if (strandA.length !== strandB.length) {
+    throw new Error('left and right strands must be of equal length')
+  }
+
+  const LENGTH = strandA.length
+  let distance = 0
+
+  // Count distance
+  for (let i = 0; i < LENGTH; i++) {
+    if (strandA[i] !== strandB[i]) distance += 1
+  }
+
+  return distance
 };
