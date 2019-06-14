4e806a5e22731db4652819b912133f8689f82a87
Joe Previte <jjprevite@gmail.com>
2019-06-14 07:25:17 -0700

    Finish rna-transcription for JS track
    This was pretty easy. I used an object with keys corresponding to each nucleotide and the value being their complement.
    
    Then I wrote a one liner that splits the string into an array, maps over and returns the complement based on the key and then joins the array back into a string.

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,5 +1,12 @@
+const COMPLEMENTS = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U'
+}
+
 export function toRna (sequence) {
   if (sequence.length === 0) return ''
   // object with key values
-
+  return sequence.split('').map(nucleotide => COMPLEMENTS[nucleotide]).join('')
 }
